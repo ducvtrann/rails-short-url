700690cacb21c264d05b82343724870bf02a3772
Duc Tran <ducvtrann@gmail.com>
2021-04-22 21:31:41 -0400

    Filled out the perform function to retrieve an url title

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,7 +1,14 @@
+require 'open-uri'
+
 class UpdateTitleJob < ApplicationJob
   queue_as :default
 
   def perform(short_url_id)
-
+    url = ShortUrl.find_by(id: short_url_id)
+    open("#{url.full_url}") do |f|
+      document = Nokogiri::HTML(f)
+      title = document.at_css('title').text
+      url.update_attribute(:title, title)
+    end
   end
 end
