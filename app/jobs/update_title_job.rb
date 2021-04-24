e4dadf8e5bc61b9c17330d04a95e00c261f13e2b
Duc Tran <ducvtrann@gmail.com>
2021-04-24 13:20:07 -0400

    Removed external gem open_uri and used system and regex match to update job title

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,15 +1,12 @@
-require 'open-uri'
-
 class UpdateTitleJob < ApplicationJob
   @queue = :update_title_job
 
   def self.perform(short_url_id)
     url = ShortUrl.find_by(id: short_url_id)
 
-    open("#{url.full_url}") do |f|
-      document = Nokogiri::HTML(f)
-      title = document.at_css('title').text
-      url.update_attribute(:title, title)
-    end
+    uri = URI(url.full_url)
+    string_result = Net::HTTP.get(uri)
+    title = string_result.match(/<title>(.*?)<\/title>/)[1]
+    url.update_attribute(:title, title)
   end
 end
